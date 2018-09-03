9173a1746522f1121aa82709d07b0d158f4eb077
fong1980 <fong1980@gmail.com>
2018-09-03 20:53:19 +0200

    checkOutOfBoarder,checkShipAllReadyPut down

--- client/src/components/setupGame.js
+++ client/src/components/setupGame.js
@@ -4,16 +4,47 @@ import { Link } from "react-router-dom";
 
 import { setupBoard } from "../actions/setupGame";
 
-import { myShips } from "../logicConstants/logicConstants";
+import {
+  myShips,
+  initalStatemyShips,
+  calcShipPositionInForState,
+  checkPutAllShip,
+  CheckOutOfBorder,
+  checkShipAllreadyPut
+} from "../logicConstants/logicConstants";
+import "./setupGame.css";
 
 // import "./game.css";
 
 class SetupGame extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { shipsMyBoard: initalStatemyShips };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.PutShip = this.PutShip.bind(this);
+  }
+  handleClick(myShipBoard, startpoint, direction, typeShip) {
+    if (!checkPutAllShip(myShipBoard)) return;
+    if (!CheckOutOfBorder(startpoint, direction, typeShip)) return;
+    if (checkShipAllreadyPut(myShipBoard, typeShip))
+      // checkCrossShip();
+
+      this.setState({
+        shipsMyBoard: calcShipPositionInForState(
+          myShipBoard,
+          startpoint,
+          direction,
+          typeShip
+        )
+      });
+
+    let a = 0;
+    this.state.zomaarOmTeVernieuwen == 0 ? (a = 1) : (a = 0);
+    this.setState({
+      zomaarOmTeVernieuwen: a
+    });
   }
 
   handleInputChange(event) {
@@ -23,67 +54,110 @@ class SetupGame extends PureComponent {
   }
   render() {
     return (
-      <form>
-        <label>
-          <p> Chose direction </p>
-          <input
-            name="directions"
-            type="radio"
-            value="H"
-            onChange={this.handleInputChange}
-          />
-          horizontal
-          <input
-            name="directions/"
-            type="radio"
-            value="V"
-            onChange={this.handleInputChange}
-          />
-          vertical
-        </label>
+      <div className="mainWrapper">
+        <div className="optionsWrapper">
+          <form>
+            <label>
+              <p> Chose direction </p>
+              <input
+                name="directions"
+                type="radio"
+                value="X"
+                onChange={this.handleInputChange}
+              />
+              horizontal
+              <input
+                name="directions"
+                type="radio"
+                value="Y"
+                onChange={this.handleInputChange}
+              />
+              vertical
+            </label>
 
-        <label>
-          <p> Chose Ships </p>
-          <input
-            name="typeShips/field"
-            type="radio"
-            value="V"
-            onChange={this.handleInputChange}
-          />
-          Vliegdekschips
+            <label>
+              <p> Chose Ships </p>
+              <input
+                name="typeShip"
+                type="radio"
+                value="V"
+                onChange={this.handleInputChange}
+              />
+              Vliegdekschips (5)
+              <br />
+              <input
+                name="typeShip"
+                type="radio"
+                value="S"
+                field="test"
+                onChange={this.handleInputChange}
+              />
+              Slagschip (4)
+              <br />
+              <input
+                name="typeShip"
+                type="radio"
+                value="K"
+                onChange={this.handleInputChange}
+              />
+              Kruiser (3)
+              <br />
+              <input
+                name="typeShip"
+                type="radio"
+                value="O"
+                onChange={this.handleInputChange}
+              />
+              Onderzeeër (3) <br />
+              <input
+                name="typeShip"
+                type="radio"
+                value="M"
+                onChange={this.handleInputChange}
+              />
+              Mijnenveger (2) <br />
+            </label>
+            <br />
+          </form>
+        </div>
+        <div className="fieldWrapper">
+          setBoard
           <br />
-          <input
-            name="typeShips/field"
-            type="radio"
-            value="S"
-            field="test"
-            onChange={this.handleInputChange}
-          />
-          Slagschip <br />
-          <input
-            name="typeShips/field"
-            type="radio"
-            value="K"
-            onChange={this.handleInputChange}
-          />
-          Kruiser <br />
-          <input
-            name="typeShips/field"
-            type="radio"
-            value="O"
-            onChange={this.handleInputChange}
-          />
-          Onderzeeër <br />
-          <input
-            name="typeShips/field"
-            type="radio"
-            value="M"
-            onChange={this.handleInputChange}
-          />
-          Mijnenveger <br />
-        </label>
-        <br />
-      </form>
+          {/* -------make a function 1--- */}
+          {this.state.shipsMyBoard.map((row, indexY) => {
+            return (
+              <div className="rows">
+                {row.map((colum, indexX) => {
+                  const ClicktCoordinate = [indexX, indexY];
+
+                  return (
+                    <div
+                      className="colums"
+                      key={indexX}
+                      onClick={() => {
+                        this.handleClick(
+                          this.state.shipsMyBoard,
+                          ClicktCoordinate,
+                          this.state.directions,
+                          this.state.typeShip
+                        );
+                      }}
+                    >
+                      {this.state.shipsMyBoard[indexY][indexX]}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+          {/* //---------end make a function 1------------- */}
+          <Link to={`/game`} onClick={() => this.handleClick(myShips)}>
+            // <button>go to game</button>
+            //{" "}
+          </Link>
+        </div>
+        //{" "}
+      </div>
     );
   }
 }
--- client/src/logicConstants/logicConstants.js
+++ client/src/logicConstants/logicConstants.js
@@ -45,5 +45,99 @@ export const myShips = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+export const initalStatemyShips = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
 //for backends
 export const baseUrl = "http://localhost:4000";
+
+export const calcShipPositionInForState = (
+  myShipBoard,
+  startpoint,
+  direction,
+  typeShip
+) => {
+  let fieldShipNeed = 0;
+
+  typeShip == "V" ? (fieldShipNeed = 5) : "";
+  typeShip == "S" ? (fieldShipNeed = 4) : "";
+  typeShip == "K" ? (fieldShipNeed = 3) : "";
+  typeShip == "O" ? (fieldShipNeed = 3) : "";
+  typeShip == "M" ? (fieldShipNeed = 2) : "";
+  let i = 0;
+  for (i = 0; i < fieldShipNeed; i++) {
+    if (direction == "X") {
+      myShipBoard[startpoint[1]][startpoint[0] + i] = typeShip[0];
+    } else {
+      myShipBoard[startpoint[1] + i][startpoint[0]] = typeShip[0];
+    }
+  }
+  return myShipBoard;
+};
+export const checkPutAllShip = myShipBoard => {
+  let count = 0;
+  myShipBoard.map((x, i) =>
+    x.map((y, i) => {
+      if (y !== 0) {
+        count++;
+      } else {
+      }
+    })
+  );
+  if (count > 16) {
+    alert("All ships already set");
+    return false;
+  }
+  return true;
+};
+
+export const CheckOutOfBorder = (startpoint, direction, typeShip) => {
+  let fieldShipNeed = 0;
+  typeShip == "V" ? (fieldShipNeed = 5) : "";
+  typeShip == "S" ? (fieldShipNeed = 4) : "";
+  typeShip == "K" ? (fieldShipNeed = 3) : "";
+  typeShip == "O" ? (fieldShipNeed = 3) : "";
+  typeShip == "M" ? (fieldShipNeed = 2) : "";
+
+  let length = 0;
+  if (direction == "X") {
+    length = startpoint[0] + fieldShipNeed;
+  } else {
+    length = startpoint[1] + fieldShipNeed;
+  }
+  if (length > 12) {
+    alert("Placement out of the boarder");
+    return false;
+  }
+  return true;
+};
+
+export const checkShipAllreadyPut = (myShipBoard, typeShip) => {
+  let exist = 0;
+  myShipBoard.map((x, i) =>
+    x.map((y, i) => {
+      // console.log(y, typeShip, "adadfadf");
+      if (y == typeShip) {
+        exist = true;
+      }
+    })
+  );
+  console.log(exist);
+  if (exist) {
+    alert("All ships already set");
+    return false;
+  }
+  return true;
+};
